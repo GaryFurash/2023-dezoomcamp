5e865ebdda94adc456552518b0768d0687e24417
Gary Furash <furashgary@gmail.com>
2023-05-25 07:29:36 -0700

    commit Thu May 25 07:29:36 MST 2023

--- week3/homework/week_3_h1.py
+++ week3/homework/week_3_h1.py
@@ -1,3 +1,10 @@
+"""
+Pre-reqs:
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of bucket
+"""
+
 import io
 import os
 import gzip
@@ -5,19 +12,14 @@ import requests
 import pandas as pd
 import pyarrow
 import logging
-import StringIO
 import tempfile
 from google.cloud import storage
 
-"""
-Pre-reqs:
-1. `pip install pandas pyarrow google-cloud-storage`
-2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
-3. Set GCP_GCS_bucket as your bucket or change default value of bucket
-"""
 
-init_url = ""
-bucket = ""
+class Constants:
+    source_url = ""
+    source_service = ""
+    gcp_bucket = ""
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -35,23 +37,23 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob.upload_from_filename(local_file)
 
 
-def web_to_gcs(year, service):
+def web_to_gcs(year, run_constants):
     for i in range(12):
 
         # set the base file name
         month = '0'+str(i+1)
         month = month[-2:]
-        dataset_file = f"{service}_tripdata_{year}-{month}"
+        dataset_file = f"{run_constants.source_service}_tripdata_{year}-{month}"
 
         # fetch the dataset from the web with pandas
-        dataset_url = f"{init_url}/{service}/{dataset_file}.csv.gz"
+        dataset_url = f"{run_constants.source_url}/{run_constants.source_service}/{dataset_file}.csv.gz"
         print(f"Feching URL: {dataset_url}")
         req = requests.get(dataset_url)
-        gz = gzip.GzipFile(StringIO.StringIO(req.content))
-        df = pd.read_csv(gz, compression="gzip")
-        print(df.head(2))
-        print(f"columns: {df.dtypes}")
-        print(f"rows: {len(df)}")
+        # gz = gzip.GzipFile(StringIO.StringIO(req.content))
+        # df = pd.read_csv(gz, compression="gzip")
+        # print(df.head(2))
+        # print(f"columns: {df.dtypes}")
+        # print(f"rows: {len(df)}")
 
         # local_path = os.path.join(tempfile.gettempdir(), file_name)
         # pd.DataFrame(io.StringIO(r.text)).to_csv(local_path)
@@ -68,14 +70,15 @@ def web_to_gcs(year, service):
 
 
 def main():
-    global init_url
-    global bucket
-    # init_url = "https://nyc-tlc.s3.amazonaws.com/trip+data/"
-    init_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/tag"
-    bucket = os.environ.get("GCP_GCS_BUCKET")
-    print(f"bucket: {bucket}")
-    # services = ['fhv','green','yellow']
-    web_to_gcs('2019', 'fhv')
+    """
+    Main Method
+    """
+    run_constants = Constants()
+    run_constants.source_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+    run_constants.source_service = "flv"
+    run_constants.gcp_bucket = os.environ.get("GCP_GCS_BUCKET")
+
+    web_to_gcs('2019', run_constants)
 
 
 if __name__ == "__main__":
